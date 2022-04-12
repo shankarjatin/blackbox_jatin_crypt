09610a65daad948cfcda9f62102769f1cc1c1bd9
Kush <Kush2012053@akgec.ac.in>
2022-04-12 10:04:25 -0700

    update frontend

--- assets/public/script.js
+++ assets/public/script.js
@@ -34,22 +34,15 @@ for(let i=0;i<=question.length-1;i++)
     });
 }
 let time=10800;
-let timer=document.getElementById("countdown");
+let timer=document.getElementsByClassName("countdown");
 setInterval(updatecountdown,1000);
 function updatecountdown(){
-    let hours=Math.floor(time/3600);
-    let minutes=Math.floor((time%3600)/60);
-    let seconds=Math.floor(time-3600*hours-60*minutes);
-    timer.innerHTML = `${hours} : ${minutes} : ${seconds}`;
-    time--;
-}
-let val=10800;
-let t=document.getElementById("counter");
-setInterval(updatecount,1000);
-function updatecount(){
-    let HOURS=Math.floor(val/3600);
-    let MINUTES=Math.floor((val%3600)/60);
-    let SECONDS=Math.floor(val-3600*HOURS-60*MINUTES);
-    t.innerHTML = `TIME LEFT : ${HOURS} : ${MINUTES} : ${SECONDS}`;
-    val--;
+    for(let i=0;i<=timer.length;i++)
+    {
+        let hours=Math.floor(time/3600);
+        let minutes=Math.floor((time%3600)/60);
+        let seconds=Math.floor(time-3600*hours-60*minutes);
+        timer[i].innerHTML = `${hours} : ${minutes} : ${seconds}`;
+        time--;
+    }
 }

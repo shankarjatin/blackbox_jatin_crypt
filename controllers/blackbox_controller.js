80e09b11944e4afff58f55f189c603d533746659
shankarjatin <shankarjatin1005@gmail.com>
2023-04-27 01:05:13 +0530

    question fetch logic

--- controllers/blackbox_controller.js
+++ controllers/blackbox_controller.js
@@ -1,9 +1,20 @@
 const Ques_BlackBox=require("../models/question_blackbox");
-
+const User = require("./../models/user.js");
 exports.FirstPage = (req,res)=>{
     res.render("blackbox_index")
 }
 
+exports.black_ques = async (req,res)=>{
+    let a = parseInt(req.body.num1);
+    let b = parseInt(req.body.num2);
+    let c = parseInt(req.body.num3);
+    let gamer = await User.findOne({email:req.user.email} );
+    var level1 = gamer.blackbox.level;
+    let ques_game = await Ques_BlackBox.findOne({level:level1})
+    var expression_black = ques_game.answer_expression;
+    var expression_real = eval(expression_black);
+    console.log(expression_real);
+}
 
 exports.add_question=(req,res,next)=>{
     const question_no=req.body.question_no;
--- routes/blackbox.js
+++ routes/blackbox.js
@@ -4,10 +4,17 @@ const game_controller = require("./../controllers/game_controllers.js");
 const auth_middleware = require("./../middlewares/auth_middleware.js");
 const game_middleware = require("./../middlewares/game_middleware.js");
 const controller = require("./../controllers/controller.js");
+
+
+
 const blackbox_controller = require("./../controllers/blackbox_controller.js");
 router.get("/blackbox",
 blackbox_controller.FirstPage
 )
 
 router.post("/add-question",blackbox_controller.add_question);
+
+router.post("/black_ques",blackbox_controller.black_ques);
+
+
 module.exports = router;

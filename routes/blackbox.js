a1ebc1760182d1cb01922ff6a6b921fe856db182
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-05-02 01:21:55 +0530

    removed_custome_validator

--- routes/blackbox.js
+++ routes/blackbox.js
@@ -17,15 +17,7 @@ router.post("/add-question", blackbox_controller.add_question);
 
 router.post("/black_ques",[body(`num1`).isInt({min:1}).notEmpty(),body(`num2`).isInt({min:1}).notEmpty(),body(`num3`).isInt({min:1}).notEmpty(),body(`num4`).isInt({min:1}).notEmpty()],blackbox_controller.black_ques);
 
-router.post("/submit_blackbox",[body(`user_expression`).notEmpty().trim().custom((value,{req})=>{
-    console.log(value);
-    const pattern = /^[a-zA-Z0-9]+$/; 
-    if (pattern.test(value)) {
-        console.log('Input is valid.');
-    } else {
-          return Promise.reject("Invalid Input Please Enter as per the instruction");
-    }  
-})],blackbox_controller.submit_blackbox);
+router.post("/submit_blackbox",[body(`user_expression`).notEmpty().trim()],blackbox_controller.submit_blackbox);
 
 router.get("/blackbox_leaderboard" , blackbox_controller.leaderboard);
 

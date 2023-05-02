c3326e3e04b136b876b599bc0de13d5c5fcfe598
gracygupta <gracy2112004@akgec.ac.in>
2023-05-02 22:26:31 +0530

    remove custom validator

--- routes/blackbox.js
+++ routes/blackbox.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {check,body}=require("express-validator");
+const { check, body } = require("express-validator");
 const game_controller = require("./../controllers/game_controllers.js");
 const auth_middleware = require("./../middlewares/auth_middleware.js");
 const game_middleware = require("./../middlewares/game_middleware.js");
@@ -9,16 +9,16 @@ const controller = require("./../controllers/controller.js");
 
 
 const blackbox_controller = require("./../controllers/blackbox_controller.js");
-router.get("/blackbox",game_middleware.check_game_timing,auth_middleware.check_login,
-blackbox_controller.FirstPage
+router.get("/blackbox", game_middleware.check_game_timing, auth_middleware.check_login,
+    blackbox_controller.FirstPage
 )
 
 router.post("/add-question", blackbox_controller.add_question);
 
-router.post("/black_ques",[body(`num1`).isInt({min:1}).notEmpty(),body(`num2`).isInt({min:1}).notEmpty(),body(`num3`).isInt({min:1}).notEmpty(),body(`num4`).isInt({min:1}).notEmpty()],blackbox_controller.black_ques);
+router.post("/black_ques", [body(`num1`).isInt({ min: 1 }).notEmpty(), body(`num2`).isInt({ min: 1 }).notEmpty(), body(`num3`).isInt({ min: 1 }).notEmpty(), body(`num4`).isInt({ min: 1 }).notEmpty()], blackbox_controller.black_ques);
 
-router.post("/submit_blackbox",[body(`user_expression`).notEmpty().trim()],blackbox_controller.submit_blackbox);
+router.post("/submit_blackbox", [body(`user_expression`).notEmpty().trim()], blackbox_controller.submit_blackbox);
 
-router.get("/blackbox_leaderboard" , blackbox_controller.leaderboard);
+router.get("/blackbox_leaderboard", blackbox_controller.leaderboard);
 
 module.exports = router;

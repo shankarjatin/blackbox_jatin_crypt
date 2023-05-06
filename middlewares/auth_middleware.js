23c34d170fa7f1aeda26aab4e2dd7258ad0e7969
gracygupta <gracy2112004@akgec.ac.in>
2023-05-06 17:38:44 +0530

    resolved conflicts

--- middlewares/auth_middleware.js
+++ middlewares/auth_middleware.js
@@ -10,8 +10,6 @@ exports.check_login = (req, res, next) => {
 }
 
 exports.check_admin = (req, res, next) => {
-	console.log("email", req.user.email);
-	console.log(process.env.ADMIN_1_EMAIL, process.env.ADMIN_2_EMAIL);
 	if (req.user.email == process.env.ADMIN_1_EMAIL || req.user.email == process.env.ADMIN_2_EMAIL) {
 		next();
 	}

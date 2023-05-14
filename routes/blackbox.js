ba02ec9ab526d2e97ee96086395f400723e95286
Shahbaz <shahbaz_ali@outlook.in>
2023-05-15 02:24:17 +0530

    code refactoring

--- routes/blackbox.js
+++ routes/blackbox.js
@@ -3,28 +3,44 @@ const router = express.Router();
 const { body } = require("express-validator");
 const auth_middleware = require("./../middlewares/auth_middleware.js");
 const game_middleware = require("./../middlewares/game_middleware.js");
-
-
-
 const blackbox_controller = require("./../controllers/blackbox_controller.js");
-router.get("/blackbox", game_middleware.check_game_timing, auth_middleware.check_login,
+
+router.get("/blackbox", 
+    game_middleware.check_game_timing, 
+    auth_middleware.check_login,
     blackbox_controller.getBlackbox
 )
 
+router.post("/blackbox", 
+    game_middleware.check_game_timing, 
+    auth_middleware.check_login,
+    blackbox_controller.postBlackbox
+)
 
-router.post("/blackbox", game_middleware.check_game_timing, auth_middleware.check_login,
-    blackbox_controller.postBlackbox)
-
-
-router.post("/add-question", auth_middleware.check_login,
-    auth_middleware.check_admin, blackbox_controller.add_question);
-
+router.post("/add-question", 
+    auth_middleware.check_login,
+    auth_middleware.check_admin, 
+    blackbox_controller.add_question
+);
 
 //input evaluation
-router.post("/black_ques", auth_middleware.check_login,game_middleware.check_game_timing,blackbox_controller.black_ques);
+router.post("/black_ques", 
+    auth_middleware.check_login,
+    game_middleware.check_game_timing,
+    blackbox_controller.black_ques
+);
 
-router.post("/submit_blackbox", auth_middleware.check_login,game_middleware.check_game_timing,[body(`user_expression`).notEmpty().trim()], blackbox_controller.submit_blackbox);
+router.post("/submit_blackbox", 
+    auth_middleware.check_login,
+    game_middleware.check_game_timing,
+    [
+        body(`user_expression`).notEmpty().trim()
+    ], 
+    blackbox_controller.submit_blackbox
+);
 
-router.get("/blackbox_leaderboard", blackbox_controller.leaderboard);
+router.get("/blackbox_leaderboard", 
+    blackbox_controller.leaderboard
+);
 
 module.exports = router;

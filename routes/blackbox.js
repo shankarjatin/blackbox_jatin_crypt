7eed14c343923c47f297d657bdcf520b0a7f8b85
Shahbaz <shahbaz_ali@outlook.in>
2023-05-15 02:41:56 +0530

    add check_registration to necessary blackbox routes

--- routes/blackbox.js
+++ routes/blackbox.js
@@ -8,17 +8,20 @@ const blackbox_controller = require("./../controllers/blackbox_controller.js");
 router.get("/blackbox", 
     game_middleware.check_game_timing, 
     auth_middleware.check_login,
+    auth_middleware.check_registration,
     blackbox_controller.getBlackbox
 )
 
 router.post("/blackbox", 
     game_middleware.check_game_timing, 
     auth_middleware.check_login,
+    auth_middleware.check_registration,
     blackbox_controller.postBlackbox
 )
 
 router.post("/add-question", 
     auth_middleware.check_login,
+    auth_middleware.check_registration, // admin needs to be registered as a team too !!!
     auth_middleware.check_admin, 
     blackbox_controller.add_question
 );
@@ -26,12 +29,14 @@ router.post("/add-question",
 //input evaluation
 router.post("/black_ques", 
     auth_middleware.check_login,
+    auth_middleware.check_registration,
     game_middleware.check_game_timing,
     blackbox_controller.black_ques
 );
 
 router.post("/submit_blackbox", 
     auth_middleware.check_login,
+    auth_middleware.check_registration,
     game_middleware.check_game_timing,
     [
         body(`user_expression`).notEmpty().trim()
@@ -39,7 +44,8 @@ router.post("/submit_blackbox",
     blackbox_controller.submit_blackbox
 );
 
-router.get("/blackbox_leaderboard", 
+router.get("/blackbox_leaderboard",
+    // remove this comment after making sure check_registration is not needed
     blackbox_controller.leaderboard
 );
 

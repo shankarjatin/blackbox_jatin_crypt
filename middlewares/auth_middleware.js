ec1c97dbd88c16851217fd3b632ac69e915db27f
Shahbaz <shahbaz_ali@outlook.in>
2023-05-15 15:27:43 +0530

    fix no response when not authenticated

--- middlewares/auth_middleware.js
+++ middlewares/auth_middleware.js
@@ -2,17 +2,22 @@ const User = require("./../models/user");
 const Team = require("../models/teams");
 
 exports.check_login = async (req, res, next) => {
-	if (req.isAuthenticated()) {
-		const team = await Team.findOne({ "$or": [{ leader_email: req.user.email }, { member_email: req.user.email }] });
-		if (team) {
-			req.team = team; // attaching team to req
-			next();
-		} else {
-			res.render("index", {
-				message: "You are not registered for the game.",
-				time_to_start: req.remainingTime
-			});
-		}
+	if (!req.isAuthenticated()) {
+		return res.render("index", {
+			message: "You need to log in first.",
+			time_to_start: req.remainingTime
+		});
+	}
+	
+	const team = await Team.findOne({ "$or": [{ leader_email: req.user.email }, { member_email: req.user.email }] });
+	if (team) {
+		req.team = team; // attaching team to req
+		next();
+	} else {
+		res.render("index", {
+			message: "You are not registered for the game.",
+			time_to_start: req.remainingTime
+		});
 	}
 }
 

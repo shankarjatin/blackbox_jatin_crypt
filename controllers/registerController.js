8ebbee2b93e57db85177924ab589ace0fb71c556
gracygupta <hzgracy@gmail.com>
2024-04-16 15:07:18 +0530

    add consoling

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,13 +2,20 @@ const Team = require("../models/teams");
 
 //storing team name and details of leader and member
 const register = async (req, res) => {
-    await Team.create({
-        team_name: req.body.team_name,
-        leader_name: req.body.leader_name,
-        leader_email: req.body.leader_email,
-        member_name: req.body.member_name,
-        member_email: req.body.member_email,
-    }).then(() => { res.status(200).json({ success: true }) }).catch((err) => { res.status(500).json({ success: false }) })
-}
+  await Team.create({
+    team_name: req.body.team_name,
+    leader_name: req.body.leader_name,
+    leader_email: req.body.leader_email,
+    member_name: req.body.member_name,
+    member_email: req.body.member_email,
+  })
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      res.status(500).json({ success: false });
+      console.log(err);
+    });
+};
 
 module.exports = register;
